9e17df16195e9ef15b02a273bdd5d99f54df19e4
diegofg11 <diiegofernandezg@gmail.com>
2025-09-21 21:04:51 +0200

    Initial commit

---
+++ src/main/java/org/example/Controlador.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class Controlador {
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextArea descriptionArea;
+
+    @FXML
+    private Label statusLabel;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private Button cancelButton;
+}
+
+
